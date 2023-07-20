b1713ce6695ef3242aaa1e6e40976280ac82a4ad
NoNameTechnology <129623103+NoNameTechnology@users.noreply.github.com>
2023-07-20 22:03:58 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -99,8 +99,3 @@ def visualize(
             close_servo()
             servo_state = False
   return image
-
-# Schleife zum Schließen des Servos alle 10 Sekunden
-while True:
-    close_servo()
-    time.sleep(10)
